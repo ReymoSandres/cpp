f42ada6d05941ee818515ef1665a1bae0e14717b
Reymo Sandres <Reymo.sandres@unah.hn>
2022-02-01 21:39:56 -0600

    agregando ejercicio 5 del programa suma

--- Ejercicios/ejercicio4-imprimi valores de Todas las variables/todas-las-variables.cpp
+++ Ejercicios/ejercicio4-imprimi valores de Todas las variables/todas-las-variables.cpp
@@ -10,8 +10,8 @@ int main(int argc, char const *argv[])
      int valorEntero=15;
      double valorDouble=20.99;
      string valorString= "Hola como estan";
-
      char valorChar= 'B';
+     
      cout << "valor Boolean:" <<  valorBoolean   << endl;
      cout   << "valor Entero:" <<   valorEntero   << endl;
      cout   << "valor Double:" <<   valorDouble   << endl;
